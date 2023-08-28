0478bf570cbe2fb1309e010a992b61917a836221
Firdosh01 <firdosh7392@gmail.com>
2023-08-28 02:37:32 -0500

    separate components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef } from 'react';
-import axios from 'axios';
+import Header from './components/Header';
 
-
-const API_URL = 'https://api.unsplash.com/search/photos'
-const IMAGES_PER_PAGE = 20;
 
 function App() {
-  const searchInput = useRef(null)
-  console.log(process.env.REACT_APP_API_KEY)
-
-
-  useEffect(() => {
-    const getImages = async () => {
-      try{
-      const result = await axios.get(`${API_URL}?query=${'nature'}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${process.env.REACT_APP_API_KEY}`)
-      console.log(result.data)
-      }
-      catch(error) {
-        console.log(error)
-      }
-    }
-    getImages()
-  }, [])
-
-  const handleSearch = (event) => {
-    event.preventDefault()
-    console.log(searchInput.current.value)
-  }
   
   return (
     <div>
-      <div>
-        <h1>Image Search</h1>
-        <div>
-          <form onSubmit={handleSearch} >
-            <input 
-            type="text"
-            placeholder='search image...'
-            ref={searchInput}
-             />            
-          </form>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import axios from 'axios'
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
+
 
 export default function Header() {
+
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [errorMsg, setErrorMsg] = useState('');
+  console.log(process.env.REACT_APP_API_KEY)
+  console.log(page)
+
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        const { data } = await axios.get(
+          `${API_URL}?query=${searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${process.env.REACT_APP_API_KEY
+          }`
+        );
+
+        setImages(data.results);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      console.log(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    resetSearch();
+  };
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
   return (
-    <div>Header</div>
+    <div>
+      <div className='flex flex-col items-center py-10 bg-gray-900'>
+
+        <h1 className='font-serif font-bold text-white'>Image Search</h1>
+        {errorMsg && <p className='error-msg'>{errorMsg}</p>}
+
+        <div className='mt-4 search-section'>
+          <form onSubmit={handleSearch}>
+            <input type='search'
+              placeholder='Type something to search...'
+              ref={searchInput}
+            />
+          </form>
+        </div>
+
+        <div className='flex flex-row gap-5 mt-4'>
+          <button className='px-3 py-1 text-center text-white bg-blue-600 rounded-md' onClick={() => handleSelection('nature')}>Nature</button>
+          <button className='px-3 py-1 text-center text-white bg-blue-600 rounded-md' onClick={() => handleSelection('birds')}>Birds</button>
+          <button className='px-3 py-1 text-center text-white bg-blue-600 rounded-md' onClick={() => handleSelection('cats')}>Cats</button>
+          <button className='px-3 py-1 text-center text-white bg-blue-600 rounded-md' onClick={() => handleSelection('shoes')}>Shoes</button>
+        </div>
+
+      </div>
+
+      <div>
+        {
+          <div className='grid gap-4 px-4 mx-auto my-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl'>
+            {
+              images.map((image, index) => (
+                <img
+                  key={image.id}
+                  src={image.urls.full}
+                  alt={image.alt_description}
+                  className='w-full rounded-lg shadow-md h-72'
+                />
+              ))}
+          </div>
+        }
+      </div>
+
+    </div>
   )
 }
