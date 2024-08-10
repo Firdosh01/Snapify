016a28a905a1d089a29653609d7ac53f3ae19fbb
Md Firdosh Ansari <mdfirdosha37@gmail.com>
2024-08-10 10:07:52 -0700

    responsive masonry added in images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,38 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import axios from 'axios'
-import Pagination from './Pagination';
-import Spinner from './Spinner';
-import Images from './Images';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import Pagination from "./Pagination";
+import Spinner from "./Spinner";
+import Images from "./Images";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const API_URL = 'https://api.unsplash.com/search/photos';
+const API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 20;
 
-
 export default function Header() {
-
   const searchInput = useRef(null);
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [errorMsg, setErrorMsg] = useState('');
-  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  console.log("process.env.REACT_APP_API_KEY", process.env.REACT_APP_API_KEY);
 
   const fetchImages = useCallback(async () => {
     try {
       if (searchInput.current.value) {
-        setLoading(true)
-        setErrorMsg('');
+        setLoading(true);
+        setErrorMsg("");
         const { data } = await axios.get(
-          `${API_URL}?query=${searchInput.current.value
-          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${process.env.REACT_APP_API_KEY
-          }`
+          `${API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${process.env.REACT_APP_API_KEY}`
         );
 
         setImages(data.results);
         setTotalPages(data.total_pages);
-        setLoading(false)
+        setLoading(false);
       }
     } catch (error) {
-      setErrorMsg('Error fetching images. Try again later.');
+      setErrorMsg("Error fetching images. Try again later.");
       console.log(error);
     }
   }, [page]);
@@ -60,70 +58,46 @@ export default function Header() {
 
   return (
     <div>
-      <div className='flex flex-col items-center py-10 bg-gray-900'>
-
-        <h1 className='text-2xl font-bold text-white '>Image Search</h1>
-        <div className='mt-4 search-section'>
+      <div className="flex flex-col items-center py-10 bg-gray-900">
+        <h1 className="text-xl font-bold text-white ">Search stunning photos in seconds</h1>
+        <div className="mt-4 search-section">
           <form onSubmit={handleSearch}>
-            <input type='search'
-              placeholder='Type something to search...'
+            <input
+              type="search"
+              placeholder="Type something to search..."
               ref={searchInput}
-              className='search-input'
+              className="search-input"
             />
           </form>
         </div>
 
-        <div className='flex-row hidden gap-5 mt-4 object md:flex'>
-          
-          <button 
-          className='w-[100px] text-center text-white bg-blue-600 rounded-md py-2' 
-          onClick={() => handleSelection('nature')}
-          >
-            Nature
-          </button>
-          
-          <button 
-          className='w-[100px] text-center text-white bg-blue-600 rounded-md py-2' 
-          onClick={() => handleSelection('birds')}
-          >
-            Birds
-          </button>
-          
-          <button 
-          className='w-[100px] text-center text-white bg-blue-600 rounded-md py-2' 
-          onClick={() => handleSelection('cats')}
-          >
-            Cats
-          </button>
-          
-          <button 
-          className='w-[100px] text-center text-white bg-blue-600 rounded-md py-2' 
-          onClick={() => handleSelection('shoes')}
-          >
-            Shoes
-          </button>
-          
-        </div>
-
       </div>
-      
-      {errorMsg && <p className='pt-5 font-serif text-center error-msg'>{errorMsg}</p>}
+
+      {errorMsg && (
+        <p className="pt-5 font-serif text-center error-msg">{errorMsg}</p>
+      )}
 
       <div>
-        {
-          loading && !errorMsg ? <Spinner /> :
-
-          <div className='grid gap-4 px-4 mx-auto my-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl'>
-            {
-              images.map((image) => (
-                <Images image={image} />
-              ))}
+        {loading && !errorMsg ? (
+          <Spinner />
+        ) : (
+          <div >
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+              
+            >
+              <Masonry>
+                {images.map((image) => (
+                  <Images image={image} />
+                ))}
+              </Masonry>
+            </ResponsiveMasonry>
           </div>
-        }
+        )}
       </div>
-         {
-          !loading && <Pagination page={page} setPage={setPage} totalPages={totalPages} />
-         }
+      {!loading && (
+        <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+      )}
     </div>
-  )
+  );
 }
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function Images({ image }) {
   return (
-    <div key={image.id}>
+    <div
+      key={image.id}
+      className="w-full h-full"
+    >
       <img
         src={image.urls.small_s3}
         alt={image.alt_description}
-        className="object-cover w-full rounded-lg shadow-md h-72"
+        className="w-full h-full p-2"
         loading="lazy"
       />
     </div>
