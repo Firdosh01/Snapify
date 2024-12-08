2e237147f05b6d25d715acaab2922203464ebdd2
Md Firdosh Ansari <mdfirdosha37@gmail.com>
2024-12-08 08:14:49 -0800

    clicked zoom image added

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Images({ image }) {
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  useEffect(() => {
+    if (isZoomed) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isZoomed]);
+
   return (
-    <div
-      key={image.id}
-      className="w-full h-full image-container"
-    >
-      <img
-        src={image.urls.small_s3}
-        alt={image.alt_description}
-        className="w-full h-full p-2 object-cover cursor-pointer hover:grayscale-[60%] duration-500"
-        loading="lazy"
-      />
-    </div>
+    <>
+      {!isZoomed ? (
+        <div key={image.id} className="w-full h-full image-container">
+          <img
+            src={image.urls.small_s3}
+            alt={image.alt_description}
+            className="w-full h-full p-2 object-cover cursor-pointer hover:grayscale-[60%] duration-500"
+            loading="lazy"
+            onClick={() => {
+              setIsZoomed(true);
+            }}
+          />
+        </div>
+      ) : (
+        <div
+          className="zoom-image_container"
+          onClick={() => {
+            setIsZoomed(false);
+          }}
+        >
+          <div className="scrollable-image">
+            <img src={image.urls.full} alt="img" className="zoomed-image" />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
